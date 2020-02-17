2d2955888b0bc86b0dce1cfe9971a99b572542b8
alexd <37stu37@gmail.com>
2020-02-17 17:28:57 +1300

    update

--- Dynamic_Fire.py
+++ Dynamic_Fire.py
@@ -51,7 +51,7 @@ def Wind_scenario(wind_data=wind_df):
 def Fire_propagation(fuel_map, wind_direction, critical_distance, ignition_map):
     # states hold the state of each cell
     time = 0
-    states = np.zeros(time, *ignition_map)
+    states = np.zeros((time, *ignition_map))
     states[0] = states(time, *ignition_map)
 
     # Make a copy of the original states
