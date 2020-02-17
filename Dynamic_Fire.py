d7586638aed5796754bf11a39d7e150414e52bd3
alexd <37stu37@gmail.com>
2020-02-17 16:04:29 +1300

    python version

--- Dynamic_Fire.py
+++ Dynamic_Fire.py
@@ -11,13 +11,23 @@ import random
 
 path_to_data = ''
 
-fuel_map = gpd.read_file('/content/drive/My Drive/04_Cloud/01_Work/GNS/008_FFE/merge.shp')
-fuel_map.plot()
-fuel_map.crs
-fuel_map.info()
-fuel_map.SHAPE_Area.min()
+# Load data needed
+# fuel_map = gpd.read_file('/content/drive/My Drive/04_Cloud/01_Work/GNS/008_FFE/merge.shp')
+# fuel_map.plot()
+# fuel_map.crs
+# fuel_map.info()
+# fuel_map.SHAPE_Area.min()
+# load building map as a "fuel" map
+fuel_map = gdal_array.LoadFile('FuelMapRaster.tif')
 
+# load probability of building ignition as an array
+ignition_proba = gdal_array.LoadFile('FireProba.tif')
 
-raster = gdal_array.LoadFile('/content/drive/My Drive/04_Cloud/01_Work/GNS/008_FFE/FuelMapRaster.tif')
-type(raster)
+# load wind data
+wind_df = pd.read('WindScenariosCopy.csv')
 
+def Fire_ignition(ignition_proba_array=ignition_proba):
+    random_ignition_proba_array = np.zeros_like(ignition_proba_array, float)
+    random_ignition_proba_array = np.random.uniform(low=0, high=1, size= ignition_proba_array))
+    # get position of fire
+    i, j = np.where(ignition_proba_array < random_ignition_proba_array)
