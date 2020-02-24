871c95448e9e3864450c04e4f1754386660175d5
37stu37 <60672071+37stu37@users.noreply.github.com>
2020-02-24 16:22:12 +1300

    updated time / ignition / fire map

--- firemodel_mesa.py
+++ firemodel_mesa.py
@@ -76,112 +76,23 @@ class Buildings(GeoAgent):
             self.condition = "Burned Out"
 
 
-#
-# class AgentCreator:
-#     """Create GeoAgents from files, GeoDataFrames, GeoJSON or Shapely objects."""
-#
-#     def __init__(self, agent_class, agent_kwargs, crs={"init": "epsg:2193"}):
-#         """Define the agent_class and required agent_kwargs.
-#         Args:
-#             agent_class: Reference to a GeoAgent class
-#             agent_kwargs: Dictionary with required agent creation arguments.
-#                 Must at least include 'model' and must NOT include unique_id
-#             crs: Coordinate reference system. If shapes are loaded from file
-#                 they will be converted into this crs automatically.
-#         """
-#         if "unique_id" in agent_kwargs:
-#             agent_kwargs.remove("unique_id")
-#             warnings.warn("Unique_id should not be in the agent_kwargs")
-#
-#         self.agent_class = agent_class
-#         self.agent_kwargs = agent_kwargs
-#         self.crs = crs
-#
-#     def create_agent(self, shape, unique_id):
-#         """Create a single agent from a shape and a unique_id
-#         Shape must be a valid Shapely object."""
-#
-#         if not isinstance(shape, BaseGeometry):
-#             raise TypeError("Shape must be a Shapely Geometry")
-#
-#         new_agent = self.agent_class(
-#             unique_id=unique_id, shape=shape, **self.agent_kwargs
-#         )
-#
-#         return new_agent
-#
-#     def from_GeoDataFrame(self, gdf, unique_id="index", set_attributes=True):
-#         """Create a list of agents from a GeoDataFrame.
-#         Args:
-#             unique_id: Column to use for the unique_id.
-#                 If "index" use the GeoDataFrame index
-#             set_attributes: Set agent attributes from GeoDataFrame columns.
-#         """
-#
-#         if unique_id != "index":
-#             gdf = gdf.set_index(unique_id)
-#
-#         gdf = gdf.to_crs(self.crs)
-#
-#         agents = list()
-#
-#         for index, row in gdf.iterrows():
-#             shape = row.geometry
-#             new_agent = self.create_agent(shape=shape, unique_id=index)
-#
-#             if set_attributes:
-#                 for col in row.index:
-#                     if not col == "geometry":
-#                         setattr(new_agent, col, row[col])
-#             agents.append(new_agent)
-#         return agents
-#
-#     def from_file(self, filename, unique_id="index", set_attributes=True):
-#         """Create agents from vector data files (e.g. Shapefiles).
-#         Args:
-#             filename: The filename of the vector data
-#             unique_id: The field name of the data to use as the agents unique_id
-#             set_attributes: Set attributes from data records
-#         """
-#         gdf = gpd.read_file(filename)
-#         agents = self.from_GeoDataFrame(
-#             gdf, unique_id=unique_id, set_attributes=set_attributes
-#         )
-#         return agents
-#
-#     def from_GeoJSON(self, GeoJSON, unique_id="index", set_attributes=True):
-#         """Create agents from a GeoJSON object or string.
-#         Args:
-#             GeoJSON: The GeoJSON object or string
-#             unique_id: The fieldfeature name of the data to use as the agents unique_id
-#             set_attributes: Set attributes from features
-#         """
-#         if type(GeoJSON) is str:
-#             gj = json.loads(GeoJSON)
-#         else:
-#             gj = GeoJSON
-#
-#         gdf = gpd.GeoDataFrame.from_features(gj)
-#         gdf.crs = "+init=epsg:4326"
-#         agents = self.from_GeoDataFrame(
-#             gdf, unique_id=unique_id, set_attributes=set_attributes
-#         )
-#         return agents
-#
-
 class GeoModel(Model):
-    def __init__(self):
+    def __init__(self, scenario):
         self.grid = GeoSpace()
+        self.scenario = scenario
 
         buildings_agent_kwargs = dict(model=self)
         AC = AgentCreator(agent_class=Buildings, agent_kwargs=buildings_agent_kwargs, )
         agents = AC.from_GeoDataFrame(gdf_buildings, unique_id="TARGET_FID")
         self.grid.add_agents(agents)
         self.schedule = RandomActivation(self)
-        self.dc = DataCollector({"Fine": lambda m: self.count_type(m, "Fine"),
-                                 "On Fire": lambda m: self.count_type(m, "On Fire"),
-                                 "Burned Out": lambda m: self.count_type(m, "Burned Out")})
-
+        self.datacollector = DataCollector(
+            {
+                "Fine": lambda m: self.count_type(m, "Fine"),
+                "On Fire": lambda m: self.count_type(m, "On Fire"),
+                "Burned Out": lambda m: self.count_type(m, "Burned Out"),
+            }
+        )
         # Set up ignition
         for agent in agents:
             if random.random() < agent.IgnProb_bl:
@@ -194,18 +105,17 @@ class GeoModel(Model):
         Advance the model by one step.
         '''
         self.schedule.step()
-        self.dc.collect(self)
+        self.datacollector.collect(self)
         # Halt if no more fire
         if self.count_type(self, "On Fire") == 0:
             self.running = False
 
-
-    def count_type(model, agent_condition):
+    def count_type(self, agent_condition):
         '''
         Helper method to count trees in a given condition in a given model.
         '''
         count = 0
-        for agent in model.schedule.agents:
+        for agent in self.schedule.agents:
             if agent.condition == agent_condition:
                 count += 1
         return count
