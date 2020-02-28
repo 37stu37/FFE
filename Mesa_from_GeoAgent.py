0209c299b4b8ba3745ad86c4c2345b185a55a4ee
37stu37 <60672071+37stu37@users.noreply.github.com>
2020-02-28 22:42:15 +1300

    updated time / ignition / fire map

--- Mesa_from_GeoAgent.py
+++ Mesa_from_GeoAgent.py
@@ -21,8 +21,8 @@ from mesa_geo import GeoSpace, GeoAgent, AgentCreator
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 
-# path = '/Users/alex/Google Drive/05_Sync/FFE/Mesa'
-path = "G:/Sync/FFE/Mesa"
+path = '/Users/alex/Google Drive/05_Sync/FFE/Mesa'
+# path = "G:/Sync/FFE/Mesa"
 
 # crop data
 minx, miny = 1748570, 5426959
@@ -31,83 +31,84 @@ bbox = box(minx, miny, maxx, maxy)
 
 # building point dataset
 gdf_buildings = gpd.read_file(os.path.join(path, "buildings_raw_pts.shp"), bbox=bbox)
-xmin,ymin,xmax,ymax = gdf_buildings.total_bounds
-position = np.add
-# grid proportion
-height = math.ceil(ymax-ymin)
-width = math.ceil(xmax-xmin)
+gdf_buildings.IgnProb_bl = 0.5
+# xmin,ymin,xmax,ymax = gdf_buildings.total_bounds
 
 # wind scenario
 wind = pd.read_csv(os.path.join(path, 'GD_wind.csv'))
-
 def wind_scenario(wind_data=wind):
     i = np.random.randint(0, wind_data.shape[0])
     w = wind_data.iloc[i, 2]
     d = wind_data.iloc[i, 1]
     return w, d
-
 direction, distance = wind_scenario()
 
 # plot map of agents
-fig, ax = plt.subplots(1, 1)
-gdf_buildings.plot(column='IgnProb_bl', ax=ax, legend=True)
-gdf_buildings['pos'] = tuple(zip(gdf_buildings.X, gdf_buildings.Y))
+def plot(column):
+    fig, ax = plt.subplots(1, 1)
+    gdf_buildings.plot(column=column, ax=ax, legend=True)
+plot('IgnProb_bl')
 
-# counting set up
-# def state_count(model):
-#     count_fine = [len(Building.state == "Fine") for Building in Fire.schedule.agents]
-#     count_on_fire = [len(Building.state == "OnFire") for Building in Fire.schedule.agents]
-#     count_burned = [len(Building.state == "Burned") for Building in Fire.schedule.agents]
-#     return count_fine, count_on_fire, count_burned
+def agent_value():
+    state_value = fine + fire
+    return state_value
 
+def model_value():
+    # number of burned asset
+    model_value = burned / 2
+    return model_value
 
 # ABM Model
-
-class Building(Agent):
-    def __init__(self, pos, model, init_state="Fine"):
-        super().__init__(pos, model)
-        self.ignition = gdf_buildings.IgnProb_bl
-        self.x, self.y = pos
+fine = 0
+fire = 1
+burned = 2
+class Building(GeoAgent):
+    def __init__(self, unique_id, model, shape, init_state=fine):
+        super().__init__(unique_id, model, shape)
+        self.shape = shape
         self.state = init_state
         self.spread = distance
 
     def step(self):
-        if self.state == "OnFire":
+        if self.state == fire:
             # propagate fire
-            for neighbor in self.model.grid.neighbor_iter(self, radius=distance):
-                if neighbor.state == "Fine":
-                    neighbor.state = "OnFire"
-            self.state = "Burned"
+            for neighbor in self.model.grid.get_neighbors(self, moore=True, radius=distance):
+                if neighbor.state == fine:
+                    neighbor.state = fire
+            self.state = burned
 
 
 class Fire(Model):
-    def __init__(self, Height=height, Width=width):
+    def __init__(self):
+        self.grid = GeoSpace()
         self.schedule = RandomActivation(self)
-        self.grid = Grid(Height, Width, torus=False)
-        self.datacollector = DataCollector({"Fine": lambda m: self.count_type(m, "Fine"), "OnFire": lambda m: self.count_type(m, "OnFire"), "Burned": lambda m: self.count_type(m, "Burned")})
+        self.running = True
+
+        # add agents to grid
+        ac = AgentCreator(agent_class=Building, agent_kwargs=dict(model=self))
+        buildings = ac.from_GeoDataFrame(gdf_buildings, unique_id="TARGET_FID")
+        self.grid.add_agents(buildings)
 
 
+        self.dc= DataCollector(
+            model_reporters={"Burned": burned},  # A function to call
+            agent_reporters={"OnFire": fire})
 
-        # def place_agents(self):
-        for (contents, x, y) in self.grid.coord_iter():
-            building = Building(self, (x, y))
-            for Xcoo, Ycoo in zip(gdf_buildings.X, gdf_buildings.Y):
-                # create building
-                if (Xcoo == x) and (Ycoo == y):
-                    self.grid._place_agent(building, (x, y))
-                    # ignition
-                    if random.random() < building.ignition:
-                        building.state = "OnFire"
-                    else:
-                        building.state = "Fine"
-                    self.schedule.add(building)
-
+        # set buildings on fire
+        for building in buildings:
+            if random.random() < building.IgnProb_bl:
+                building.state = "OnFire"
+            else:
+                building.state = "Fine"
+            self.schedule.add(building)
 
     def step(self):
         # advance model
         self.schedule.step()
         # collect data
-        self.datacollector.collect(self)
+        self.dc.collect(self)
+        if self.count_type(self, "On Fire") == 0:
+            self.running = False
 
     def count_type(model, agent_condition):
         count = 0
@@ -115,6 +116,7 @@ class Fire(Model):
             if building.state == agent_condition:
                 count += 1
         return count
+
 
 # plot results
 fire = Fire()
