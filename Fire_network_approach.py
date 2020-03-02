a9eb5e518f7ab135cb0bad56b587ae40fdb3e1a1
alexd <37stu37@gmail.com>
2020-03-02 20:49:10 +1300

    stack overflow - most logical option

--- Fire_network_approach.py
+++ Fire_network_approach.py
@@ -1,3 +1,4 @@
+import math
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -49,28 +50,37 @@ def plot(df, column_df):
     plt.show()
 
 
-b = load_data("buildings_raw_pts.shp")
-
-
-def build_edge_list(gdf=b):
-    # source = pd.DataFrame(gdf, copy=True)
-    source = gdf.copy()
+def build_edge_list(gdf=None):
+    source = pd.DataFrame(gdf, copy=True)
+    # source = gdf.copy()
     source['id'] = source.index
     target = source.copy()
-    source.columns = ['source_' + str(col)for col in source.columns]
+    source.columns = ['source_' + str(col) for col in source.columns]
     target.columns = ['target_' + str(col) for col in target.columns]
     source.rename(columns={'source_id': 'source'}, inplace=True)
     target.rename(columns={'target_id': 'target'}, inplace=True)
-    source = source[['source_IgnProb_bl', 'source_X', 'source_Y', 'source_LON', 'source_LAT', 'source_geometry', 'source']]
-    target = target[['target_IgnProb_bl', 'target_X', 'target_Y', 'target_LON', 'target_LAT', 'target_geometry', 'target']]
+    source = source[
+        ['source_IgnProb_bl', 'source_X', 'source_Y', 'source_LON', 'source_LAT', 'source_geometry', 'source']]
+    target = target[
+        ['target_IgnProb_bl', 'target_X', 'target_Y', 'target_LON', 'target_LAT', 'target_geometry', 'target']]
     list_of_dataframes = []
     tmp = target.copy()
-    for r in tmp.index:
+    for index, row in source.iterrows():
+
+
+
+    for i in source.index:
         for c in source.columns:
-            tmp[c] = source[r][c]
-            tmp['distance'] = calculate_distance(source.source_geometry, target.target_geometry)
-            tmp = tmp[tmp.distance < 45]
+            n = source.columns.get_loc(c)
+            tmp[c] = source.iloc[i][n]
+            # print(tmp.head(10))
+            # tmp['distance'] = np.sqrt(((source.source_X - target.target_X) ** 2) + ((source.source_Y - target.target_Y) ** 2))
+            # # tmp = tmp[tmp.distance < 45]
             list_of_dataframes.append(tmp)
             tmp = target.copy()
-    edge = pd.concat(list_of_dataframes)
-    return edge
+    c = pd.concat(list_of_dataframes)
+    return c, target, source
+
+
+gdf = load_data("buildings_raw_pts.shp")
+edge, target, source = build_edge_list(gdf)
