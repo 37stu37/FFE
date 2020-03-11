eda1d738efe933dc40a63e9861f56eecfe271fee
alexd <37stu37@gmail.com>
2020-03-12 07:54:00 +1300

    serious issues with the results - Do not use !

--- Fire_network_one_file_run.py
+++ Fire_network_one_file_run.py
@@ -9,11 +9,9 @@ import geopandas as gpd
 from shapely.geometry import box
 import networkx as nx
 from shapely.geometry import Point
-
-# import imageio
+import imageio
 
 pd.options.mode.chained_assignment = None  # default='warn'
-pd.option_context('display.max_rows', None, 'display.max_columns', None)
 
 path = "G:/Sync/FFE/Mesa"
 path_output = "G:\Sync\FFE\FireNetwork"
@@ -23,11 +21,13 @@ path_output = "G:\Sync\FFE\FireNetwork"
 # path_output = '/Users/alex/Google Drive/05_Sync/FFE/Mesa/output'
 
 
-def load_data(file_name):  # , minx, miny, maxx, maxy):
+# path = '/Users/alex/Google Drive/05_Sync/FFE/Mesa'
+
+def load_data(file_name, minx, miny, maxx, maxy):
     # crop data
-    # bbox = box(minx, miny, maxx, maxy)
+    bbox = box(minx, miny, maxx, maxy)
     # building point dataset
-    gdf_buildings = gpd.read_file(os.path.join(path, file_name))  # , bbox=bbox)
+    gdf_buildings = gpd.read_file(os.path.join(path, file_name), bbox=bbox)
     # gdf_buildings.IgnProb_bl = 0.02
     # xmin,ymin,xmax,ymax = gdf_buildings.total_bounds
     return gdf_buildings
@@ -67,33 +67,27 @@ def build_edge_list(geodataframe, maximum_distance, polygon_file):
     df = pd.DataFrame()
     df['source_id'] = source
     df['target_id'] = target
-    # print("df : {}".format(df))
-    # copy geodataframe with index
-    geo_df = geodataframe.copy().reset_index()
+    # merge source attributes with source index
+    geo_df = geodataframe.copy()
     geo_df['id'] = geo_df.index
-    # print("geo_df : {}".format(geo_df))
-    # print("geo_df.id : {}".format(geo_df.id))
     # create source / target gdf from gdf.columns of interest
     geo_df = geo_df[['id', 'TARGET_FID', 'X', 'Y', 'geometry', 'IgnProb_bl']]
     geo_df_TRG = geo_df.copy()
     geo_df_TRG.columns = ['target_' + str(col) for col in geo_df_TRG.columns]
-    # print("geo_df_TRG : {}".format(geo_df_TRG))
     geo_df_SRC = geo_df.copy()
     geo_df_SRC.columns = ['source_' + str(col) for col in geo_df_SRC.columns]
-    # print("geo_df_SRC : {}".format(geo_df_SRC))
     # merge data
     merged_data = pd.merge(df, geo_df_SRC, left_on='source_id', right_on='source_id', how='outer')
-    # print("merged data : {} ".format(merged_data))
-    merged_data.isnull().sum()
     merged_data = pd.merge(merged_data, geo_df_TRG, left_on='target_id', right_on='target_id', how='outer')
     merged_data.rename(columns={'source_id': 'source', 'target_id': 'target'}, inplace=True)
-    # print("merged data : {} ".format(merged_data))
-    merged_data.isnull().sum()
     # calculate distance for each source / target pair
     # create a df from polygon shape to get accurate distance
+    # print(list(polygon_file))
     polygon = polygon_file[['TARGET_FID', 'geometry']]
+    # print(list(polygon))
     source_poly = merged_data[['source_TARGET_FID']]
     target_poly = merged_data[['target_TARGET_FID']]
+    # print(list(source_poly))
     src_poly = pd.merge(source_poly, polygon, left_on='source_TARGET_FID', right_on='TARGET_FID', how='left')
     trg_poly = pd.merge(target_poly, polygon, left_on='target_TARGET_FID', right_on='TARGET_FID', how='left')
     src_poly_gdf = gpd.GeoDataFrame(src_poly, geometry='geometry')
@@ -118,10 +112,10 @@ def build_edge_list(geodataframe, maximum_distance, polygon_file):
 
 def create_network(edge_list_dataframe):
     graph = nx.from_pandas_edgelist(edge_list_dataframe, edge_attr=True)
-    # options = {'node_color': 'red', 'node_size': 50, 'width': 1, 'alpha': 0.4,
-    #            'with_labels': False, 'font_weight': 'bold'}
-    # nx.draw_kamada_kawai(graph, **options)
-    # plt.show()
+    options = {'node_color': 'red', 'node_size': 50, 'width': 1, 'alpha': 0.4,
+               'with_labels': False, 'font_weight': 'bold'}
+    nx.draw_kamada_kawai(graph, **options)
+    plt.show()
     return graph
 
 
@@ -198,7 +192,7 @@ def fire_spreading(list_fires, list_burn, wind_speed, wind_bearing, suppression_
     return list_fires, list_burn
 
 
-def log_files_concatenate(prefix, scenario_count, geographical_region):
+def log_files_concatenate(prefix, scenario_count):
     list_df = []
     files = glob.glob(os.path.join(path_output, prefix))
     if files:
@@ -209,7 +203,7 @@ def log_files_concatenate(prefix, scenario_count, geographical_region):
             os.remove(file)
         data = pd.concat(list_df)
         data['scenario'] = scenario_count
-        data.to_csv(os.path.join(path_output, "{}_fire_scenario_{}.csv".format(geographical_region, scenario_count)))
+        data.to_csv(os.path.join(path_output, "fire_scenario_{}.csv".format(scenario_count)))
     else:
         print("no files to concatenate")
 
@@ -221,7 +215,7 @@ def clean_up_file(prefix, path_path=path_output):
         os.remove(file)
 
 
-def postprocessing(scenarios_recorded, burned_asset, edge_list, gdf_polygons, geographical_region):
+def postprocessing(scenarios_recorded, burned_asset, edge_list, gdf_polygons):
     list_of_tuples = list(zip(scenarios_recorded, burned_asset))
     df = pd.DataFrame(list_of_tuples, columns=['scenarios', 'burned_asset_index'])
     # df['count'] = df['burned_asset_index'].value_counts().values
@@ -239,123 +233,86 @@ def postprocessing(scenarios_recorded, burned_asset, edge_list, gdf_polygons, ge
     fig, ax = plt.subplots(1, 1)
     df_count.plot(column='count', cmap='RdYlBu_r', ax=ax, legend=True)
     ax.title.set_text("Burned buildings after {} scenarios".format(max(scenarios_recorded)))
-    plt.savefig(os.path.join(path_output, "results_{}.png".format(geographical_region)))
-    plt.close(fig)
-    # plt.show()
-    df_count = df_count.drop(columns=['source', 'source_X', 'source_Y', 'source_geometry'])
-    df_count.to_csv(os.path.join(path_output, "results_{}.csv".format(geographical_region)))
+    plt.show()
+    df_count = df_count.drop(columns=['source', 'source_TARGET_FID', 'source_X', 'source_Y', 'source_geometry'])
+    df_count.to_csv(os.path.join(path_output, "results.csv"))
     # df_count.to_file(os.path.join(path_output, "results.shp"))
     return df_count, dataframe
 
 
-def concatenate_all_results_in_shapefile(prefix, scenario_count):
-    list_df = []
-    files = glob.glob(os.path.join(path_output, prefix))
-    if files:
-        for file in files:
-            # print(file)
-            df = pd.read_csv(os.path.join(path_output, file))
-            list_df.append(df)
-        data = pd.concat(list_df)
-        data.to_csv(os.path.join(path_output, "Allresults_fire_scenario_{}.csv".format(scenario_count)))
-        gdf_data = gpd.GeoDataFrame(data)
-        return gdf_data
+# set up
+# gdf = load_data("buildings_raw_pts.shp", 1748570, 5426959, 1748841, 5427115)
+gdf_polygon = load_data("buildings_raw.shp", 1748000, 5424148, 1750000, 5427600)
+gdf_polygon["area"] = gdf_polygon['geometry'].area  # m2
+gdf = gdf_polygon.copy()
+gdf['geometry'] = gdf['geometry'].centroid
+gdf['X'] = gdf.centroid.x
+gdf['Y'] = gdf.centroid.y
+gdf['d_short'] = gdf_polygon.exterior.distance(gdf)
+gdf['d_long'] = gdf['area'] / gdf['d_short']
 
+# create edge list and network
+edges = build_edge_list(gdf, 45, gdf_polygon)
 
-# Load data
-entire_gdf_polygon = load_data("buildings_raw.shp")  # , 1740508, 5420049, 1755776, 5443033) # full area
-print("{} assets loaded".format(len(entire_gdf_polygon)))
-# entire_gdf_polygon = load_data("buildings_raw.shp", 1748000, 5424148, 1750000, 5427600)
-entire_gdf_polygon["area"] = entire_gdf_polygon['geometry'].area  # m2
-entire_gdf = entire_gdf_polygon.copy()
-entire_gdf['geometry'] = entire_gdf['geometry'].centroid
-entire_gdf['X'] = entire_gdf.centroid.x
-entire_gdf['Y'] = entire_gdf.centroid.y
-entire_gdf['d_short'] = entire_gdf_polygon.exterior.distance(entire_gdf)
-entire_gdf['d_long'] = entire_gdf['area'] / entire_gdf['d_short']
+print("{} assets loaded".format(len(gdf)))
+fig, ax = plt.subplots(2, 2)
+# gdf.plot(column='area', cmap='hsv', ax=ax[0, 0], legend=True)
+gdf_polygon.plot(column='area', cmap='hsv', ax=ax[0, 0], legend=True)
+# gdf.plot(column='TARGET_FID', cmap='hsv', ax=ax[1, 0], legend=True)
+ax[0,0].title.set_text("area")
+# ax[0,1].title.set_text("area")
+# ax[1,0].title.set_text('FID')
+plt.tight_layout()
+plt.show()
+
+# create edges
+G = create_network(edges)
 
 #################################
-
 clean_up_file("*csv")
-clean_up_file("*png")
-number_of_scenarios = 101
+number_of_scenarios = 10
 scenarios_list = []
 log_burned = []  # no removing duplicate
-list_suburb = list(entire_gdf_polygon.suburb_loc.drop_duplicates())
-
-# Turn interactive plotting off
-plt.ioff()
 # --- SCENARIOS
 t = datetime.datetime.now()
-
-for suburb in list_suburb:
-    ts = datetime.datetime.now()
-    # initiate dataset
-    gdf = entire_gdf[entire_gdf.suburb_loc == suburb]
-    gdf_polygon = entire_gdf_polygon[entire_gdf_polygon['suburb_loc'] == suburb]
-    print("{} (points) {} (polygons) assets subset loaded for the {} area".format(len(gdf), len(gdf_polygon), suburb))
-    # create edge list and network based on suburb buildings
-    edges = build_edge_list(gdf, 45, gdf_polygon)
-    G = create_network(edges)
-
-    # plot
-    fig, ax = plt.subplots(2, 2)
-    gdf.plot(column='area', cmap='hsv', ax=ax[0, 0], legend=True)
-    gdf_polygon.plot(column='area', cmap='hsv', ax=ax[0, 1], legend=True)
-    gdf.plot(column='TARGET_FID', cmap='hsv', ax=ax[1, 0], legend=True)
-    options = {'node_color': 'red', 'node_size': 50, 'width': 1, 'alpha': 0.4,
-               'with_labels': False, 'font_weight': 'bold'}
-    nx.draw_kamada_kawai(G, **options, ax=ax[1, 1])
-    ax[0, 0].title.set_text("area")
-    ax[0, 1].title.set_text("area")
-    ax[1, 0].title.set_text('FID')
-    plt.tight_layout()
-    plt.savefig(os.path.join(path_output, "inputs_{}.png".format(suburb)))
-    # plt.show()
-    plt.close(fig)
-
-    # start scenarios
-    for scenario in range(number_of_scenarios):
-        t0 = datetime.datetime.now()
-        burn_list = []
-        print("--- SCENARIO : {}".format(scenario))
-        # print("initiate fire")
-        fire_list = set_initial_fire_to(edges)
-        x = fire_list
+for scenario in range(number_of_scenarios):
+    t0 = datetime.datetime.now()
+    burn_list = []
+    print("--- SCENARIO : {}".format(scenario))
+    # print("initiate fire")
+    fire_list = set_initial_fire_to(edges)
+    x = fire_list
+    # print("fire list : {}, length : {}".format(fire_list, len(fire_list)))
+    # print("fires list in scenario loop: {}, length : {}".format(fire_list, len(fire_list)))
+    if len(fire_list) == 0:
+        print("no fire")
+        continue
+    w_direction, w_speed, w_bearing = wind_scenario()
+    # print(("critical distance : {}, wind bearing : {}".format(w_speed, w_bearing)))
+    # --------- STEPS
+    for step in range(len(edges)):
+        print("--------- STEP : {}".format(step))
+        fire_list = set_fire_to(edges, fire_list)
+        y = fire_list
+        # print("fire datasets are identical with initial fire : {}".format(set(x) == set(y)))
         # print("fire list : {}, length : {}".format(fire_list, len(fire_list)))
-        # print("fires list in scenario loop: {}, length : {}".format(fire_list, len(fire_list)))
+        # print("burn list : {}, length : {}".format(burn_list, len(burn_list)))
+        # print("spread fire")
+        fire_list, burn_list = fire_spreading(fire_list, burn_list, w_speed, w_bearing, 0, step, edges)
         if len(fire_list) == 0:
-            print("no fire")
-            continue
-        w_direction, w_speed, w_bearing = wind_scenario()
-        # print(("critical distance : {}, wind bearing : {}".format(w_speed, w_bearing)))
-        # --------- STEPS
-        for step in range(len(edges)):
-            print("--------- STEP : {}".format(step))
-            fire_list = set_fire_to(edges, fire_list)
-            y = fire_list
-            # print("fire datasets are identical with initial fire : {}".format(set(x) == set(y)))
-            # print("fire list : {}, length : {}".format(fire_list, len(fire_list)))
-            # print("burn list : {}, length : {}".format(burn_list, len(burn_list)))
-            # print("spread fire")
-            fire_list, burn_list = fire_spreading(fire_list, burn_list, w_speed, w_bearing, 0, step, edges)
-            if len(fire_list) == 0:
-                # print("no fires")
-                break
-            # print("fires list : {}, length : {}".format(fire_list, len(fire_list)))
-            # print("burn list : {}, length : {}".format(burn_list, len(burn_list)))
-        log_burned.extend(burn_list)
-        scenarios_list.extend([scenario] * len(burn_list))
-        # print("log all burn list : {}, length : {}".format(log_burned, len(log_burned)))
-        # print(scenarios_list)
+            # print("no fires")
+            break
+        # print("fires list : {}, length : {}".format(fire_list, len(fire_list)))
+        # print("burn list : {}, length : {}".format(burn_list, len(burn_list)))
+    log_burned.extend(burn_list)
+    scenarios_list.extend([scenario] * len(burn_list))
+    # print("log all burn list : {}, length : {}".format(log_burned, len(log_burned)))
+    # print(scenarios_list)
 
-        log_files_concatenate('step*', scenario, suburb)
-        t1 = datetime.datetime.now()
-        print("..... took : {}".format(t1 - t0))
-    t2 = datetime.datetime.now()
-    print("total time for the {} suburb : {}".format(suburb, t2 - ts))
-    count_gdf, count_df = postprocessing(scenarios_list, log_burned, edges, gdf_polygon, suburb)
-t3 = datetime.datetime.now()
-print("total time for all suburbs : {}".format(t3 - t))
+    log_files_concatenate('step*', scenario)
+    t1 = datetime.datetime.now()
+    print("..... took : {}".format(t1 - t0))
+t2 = datetime.datetime.now()
+print("total time : {}".format(t2 - t))
 
-gdf_final_1KFFE = concatenate_all_results_in_shapefile("*results*.csv", number_of_scenarios)
+count_gdf, count_df = postprocessing(scenarios_list, log_burned, edges, gdf_polygon)
