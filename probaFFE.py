4cb2390cf0e3609c7889bcf1c097991327b8da67
add <60672071+37stu37@users.noreply.github.com>
2020-08-24 17:12:08 +1200

    spyder

--- probaFFE.py
+++ probaFFE.py
@@ -34,14 +34,14 @@ pbar.register()
 # district data
 sa = gpd.read_file('data/shapefile/Finn_SA2Summary.shp')
 sa = sa[['BldgFloo_1', 'PopDensity', 'NumberBldg', 'geometry']] # drop column not used in caluclation of Ignition
-sa = gpd.GeoDataFrame(sa)
-sa_dask = dg.from_geopandas(sa, npartitions=4)
+sa = gpd.GeoDataFrame(sa, crs="EPSG:2193")
+sa_dask = dg.from_geopandas(sa, npartitions=8)
 del sa
 
 # pga events
 pga = pd.read_parquet('/Users/alex/Dropbox/Work/Repository/OpenQuake/output/PGAxy.parquet')
 pgaGeo = gpd.GeoDataFrame(pga, geometry=gpd.points_from_xy(pga.lon, pga.lat, crs="EPSG:2193"))
-pga_dask = dg.from_geopandas(pgaGeo, npartitions=4)
+pga_dask = dg.from_geopandas(pgaGeo, npartitions=8)
 del pga
 del pgaGeo
 
@@ -54,4 +54,6 @@ def dask_sjoin(dd1, dd2):
 PGA_SA = dd.map_partitions(dask_sjoin, pga_dask, sa_dask)
 
 #%%
-PGA_SA.to_parquet('/Users/alex/Dropbox/Work/GNS/008_FFE/ProbaFFE/PGA_SA.parquet')
+PGA_SA.compute()
+#%%
+PGA_SA.to_file('/Users/alex/Dropbox/Work/GNS/008_FFE/ProbaFFE/PGA_SA.parquet')
